424dd57b3de1b4ad3e2a33e10fdebe1e20b5cc04
c-grattan <56635397+c-grattan@users.noreply.github.com>
2022-01-21 11:40:39 +0000

    Split tasks across two files

---
+++ calc_scores.py
@@ -0,0 +1,15 @@
+from calc_stats import getData
+import sys
+
+#Load stats
+
+#Calculate scores
+def getHullData(hullsFolder):
+	hulls_folder = Path(hullsFolder)
+	return [json.load(open(shipFile)) for shipFile in hulls_folder.glob("*.ship")]
+
+def removeHiddenMounts(slots):
+	return [slot for slot in slots if slot['mount'] != 'HIDDEN']
+
+ship_data = getData(sys.argv[1] + "\\starsector-core\\data\\hulls\\ship_data.csv")
+hull_data = getHullData(sys.argv[1] + "\\starsector-core\\data\\hulls")
--- calc_stats.py
+++ calc_stats.py
@@ -1,12 +1,14 @@
-from array import array
 import csv
-from webbrowser import get
 
 def getData(file):
 	with open(file, newline='\n') as csvfile:
 		reader = csv.DictReader(csvfile)
 		return [row for row in reader]
 
-ship_data = getData("ship_data.csv")
+#Get average weapon OP costs and flux/s
 weapon_data = getData("weapon_data.csv")
-wing_data = getData("wing_data.csv")
+
+#Get average fighter costs
+wing_data = getData(sys.argv[1] + "\\starsector-core\\data\\hulls\\wing_data.csv")
+
+#Get hull mod costs
